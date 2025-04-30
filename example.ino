d53fed725f64e0d94fa4f57aacca8d3c5a72512c
tranhgocode <162459491+tranhgocode@users.noreply.github.com>
2025-04-30 22:19:28 +0700

    Update example.ino

--- example.ino
+++ example.ino
@@ -28,19 +28,29 @@ void setup() {
 
 void loop() {
   //scrollStrNgang("A");
+  //delay(500);
+  
   //scrollStrDoc("A");
-  //LedXoay(2, 50); // de 250 cho da nhin
+  //delay(500);
+  
+  //LedXoay(2, 50); // de 250 cho de nhin
+  //delay(500);
+  
   //randomFlicker(500, 10);  // 2000ms, tỉ lệ nháy 10% của 2 ma trận led (50)
   //delay(500);
   
-  // Hiệu ứng nháy điểm ảnh ngẫu nhiên
   //randomPixelFlicker(500, 2);  // 2000ms, 10 điểm sáng
   //delay(500);
 
-  HieuUngGiotNuoc(2, 100); // 2 chu kỳ, tốc độ 100ms
+  //HieuUngGiotNuoc(2, 100); // 2 chu kỳ, tốc độ 100ms
   //delay(500);
   
-  // Hiệu ứng mưa
   //HieuUngMua(2000, 2); // Hiển thị trong 2000ms, mật độ 2
+  //delay(500);
+
+  //LEDTheoHang(2, 50, 300);
+  //delay(500);
+  
+  //BanCoXenKeTuTren(2, 200);
   //delay(500);
 }
